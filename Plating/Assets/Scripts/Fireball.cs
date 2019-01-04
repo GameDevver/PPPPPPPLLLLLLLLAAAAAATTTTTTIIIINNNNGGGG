60aa608500687398357c6b374c749cda7c8f06c2
GamDevv <borsik.gr@gmail.com>
2019-01-04 15:15:30 +0300

    update

---
+++ Plating/Assets/Scripts/Fireball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public int damage = 1;
+
+    void Update()
+    {
+        transform.Translate(0, 0, - speed * Time.deltaTime);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {//вызывается когда с триггером сталкивается другой объект
+        PlayerCharacter player = other.GetComponent<PlayerCharacter>();
+        if (player != null)
+        {//проверяем, является ли объект PlayerCharacter
+            player.Hurt(damage);
+            Debug.Log("111");
+        }
+        Destroy(this.gameObject);
+    }
+}
